35a0c659335dd855828db15c4f0e29e8282da91c
Justin Dearing <zippy1981@gmail.com>
2013-06-01 12:53:26 -0400

    fix to install allplugins

--- Far-3/tools/chocolateyInstall.ps1
+++ Far-3/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ if (Test-Path $uninstallKey) {
 }
 else {
     Install-ChocolateyPackage `
-        "Far-3" 'msi' '/quiet ADDLOCAL=Addons,Colors,Macros,SetUp,Shell,XLat,FARCmds' `
+        "Far-3" 'msi' '/quiet ADDLOCAL=Addons,Colors,Macros,SetUp,Shell,XLat,arclite,FARCmds,NetBox,Compare,EMenu,FarColorer,FileCase,Network,HlfViewer,Proclist,Editor,LuaMacro,TmpPanel' `
         "http://farmanager.com/files/Far30b3367.x86.20130426.msi" `
         "http://farmanager.com/files/Far30b3367.x64.20130426.msi"
 }
--- Far-3/tools/chocolateyUninstall.ps1
+++ Far-3/tools/chocolateyUninstall.ps1
@@ -1,2 +1,2 @@
 ﻿$msiId = '{AAF36915-D499-429A-B3E7-0AF081AE0523}'
-Uninstall-ChocolateyPackage 'Far' 'MSI' "$msiId" -validExitCodes @(0)
+Uninstall-ChocolateyPackage 'Far' 'MSI' "$msiId /qb" -validExitCodes @(0)
