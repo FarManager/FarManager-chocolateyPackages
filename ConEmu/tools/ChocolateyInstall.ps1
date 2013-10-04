6070de0550dfd38712cfba0e3985e5b499e6f4de
Justin Dearing <zippy1981@gmail.com>
2013-10-04 07:21:39 -0400

    updated conemu

--- ConEmu/tools/ChocolateyInstall.ps1
+++ ConEmu/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'conemu' 'exe' '/quiet' 'http://conemu-maximus5.googlecode.com/files/ConEmuSetup.120417.exe'
+Install-ChocolateyPackage 'conemu' 'exe' '/quiet' 'http://conemu-maximus5.googlecode.com/files/ConEmuSetup.130929.exe'
